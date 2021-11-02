6b18b813c5d062625ebe549111eccd9b081609e0
unknown <hdmoviez1234@gmail.com>
2021-11-02 04:27:37 -0800

    js obj chp 1 to 13

---
+++ app.js
@@ -0,0 +1,311 @@
+///1st Chapter//
+
+// alert("Error ! Please enter a valid password");
+// alert("Welcome to JS land Happy Codding");
+// alert("Hello...I can run JS through my web browser's console")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2nd Chapter
+
+// var username = "username";
+// var fullName = "Talha Khan";
+// var JS = "Hello World";
+// var email = "My email address is example@example.com";
+// alert(email)
+// var book = "A smarter way to learn JavaScript";
+// alert(book);
+// document.write("Yeah! I can write HTML content through JavaScript")
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+// 3rd Chapter
+
+// var age = "I am 17 years old";
+// alert(age);
+// var birth = "My birth year is 2004"
+// document.write(birth)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 4th Chapter
+
+// legal variables
+// var $name
+// var P_name
+// var address1
+// var a_variable_is
+
+// illegal variables
+// var first name
+// var 12name
+// var !names
+// var %first
+
+
+
+// var h1 = "<h1> Rules for naming JS variables <h1/>";
+// document.write(h1);
+
+// var a = " <br /> <br /> Variable names can only contain numbers, $, and ______. For example $my_1stVariable";
+// document.write(a);
+// var b = "<br />  Variables must begin with a letter, $ or_____. For example $name, _name or name";
+// document.write(b);
+// var c = "<br /> Variable names are case sensitive";
+// document.write(c)
+// var d = "<br/>  Variable names should not be JS keywords"
+// document.write(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5th Chapter
+// var a = 3;
+// var b = 5;
+// var c = a + b
+// document.write("Sum of 3 and 5 is" + " " + c)
+// var a = 3;
+// var b = 5;
+// var c = a - b
+// document.write("<br /> Subtraction of 3 and 5 is" + " " + c)
+// var a = 3;
+// var b = 5;
+// var c = a * b
+// document.write("<br /> Multiplication of 3 and 5 is" + " " + c)
+// var a = 3;
+// var b = 5;
+// var c = a / b
+// document.write("<br /> Division of 3 and 5 is" + " " + c)
+
+
+// var a = 5;
+// var b = a++ + a ;
+// document.write("intial value is = 5 ");
+// document.write("<br /> after post increment = " + " " + a);
+// document.write("<br /> after post increment and addition of 5 it's become =" + b);
+
+// var a = 5;
+// var b = ++a  ;
+// document.write("<br /> intial value is = 5 ");
+// document.write("<br /> after per increment = " + " " + b);
+
+// var a = 5;
+// var b = a-- - a ;
+// document.write("intial value is = 5 ");
+// document.write("<br /> after post decrement = " + " " + a);
+// document.write("<br /> after post decrement and subtraction of 5 it's become =" + b);
+
+// var a = 5;
+// var b = --a  ;
+// document.write("<br /> intial value is = 5 ");
+// document.write("<br /> after per increment = " + " " + b);
+
+// var tickets = 5;
+// var price = 600;
+// var total = tickets * price;
+// document.write("Total cost of 5 tickets is =" + " " + total + "PKR");
+
+
+// var a = 500;
+// var b = 400;
+// var c = 600;
+// var d = 200;
+// var e = 300;
+// var d = a+b+c+d+e;
+// document.write(d)
+
+// var obt =+prompt("Enter your obtained marks " , "your obtained marks is")
+// var total =980;
+// var per = obt / total * 100;
+// document.write("Total Marks =" + " " + total);
+// document.write("<br /> Obtained Marks =" + " " + obt);
+// document.write("<br /> percentage ="  + " " + per)
+
+
+// var dprom = +prompt("Enter your dollars")
+// var dollar = 104;
+// var convert = dprom * dollar;
+// document.write(convert);
+
+// var sprom = +prompt("Enter your riyals")
+// var riyals = 28;
+// var convert = sprom * riyals;
+// document.write(convert);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 6th to 9th Chapter
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(result);
+// document.write(--a);
+// document.write(--a - --b);
+// document.write(--a - --b + ++b);
+// document.write(--a - --b + ++b + b--);
+
+
+// var names = prompt("Enter your name");
+// document.write(names);
+
+// var sub1 = +prompt("Enter your english marks");
+// var sub2 = +prompt("Enter your islamiat marks");
+// var sub3 = +prompt("Enter your javascript marks");
+// var sub4 = +prompt("Enter your html marks");
+// var totolMarks = sub1 + sub2 + sub3 + sub4;
+// var per = totolMarks / 400 * 100;
+// document.write("English =" + " " + sub1)
+// document.write("<br /> Islamiat =" + " " + sub1)
+// document.write("<br />  JavaScript =" + " " + sub3)
+// document.write("<br />  HTML =" + " " + sub4)
+// document.write("<br/> Obtain Marks:"   + totolMarks);
+// document.write("<br/> Percentage: "  + per);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 9th to 11th Chapter
+// var cname = prompt("Enter your city name");
+// if(cname == "karachi"){
+//  document.write("Welcome to city of lights")
+// }
+// else{
+//     document.write(cname)
+// }
+
+
+// var gender = prompt("Enter your gender");
+// if(gender == "male"){
+//  document.write("Good Morning Sir")
+// }
+// else if(gender == "female"){
+//     document.write("Good Morning Ma'am")
+// }
+// else{
+//     document.write("put right information")
+// }
+
+
+// var colour = prompt("Enter colour of signal");
+// if (colour == "red") {
+//     document.write("Must Stop")
+// }
+// else if (colour == "yellow") {
+//     document.write("Ready to move")
+// }
+// else {
+//     document.write("Move now")
+// }
+
+
+// var fuel = +prompt("Enter your remaining fuel");
+// if (fuel === 0.25 ) {
+//     document.write("Please refill the fuel in your car")
+// }
+// else {
+//     document.write("You have fuel")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 11th to 13th Chapter
+// var hour = prompt("Enter your time");
+// if (hour <= 12) {
+// alert(greeting = "Good day")
+// }
+// else{
+//     alert("Good evening")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
